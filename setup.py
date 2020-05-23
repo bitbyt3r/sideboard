48c9e3c7ff21bb71117411b0de67e69b9285a481
Mark Murnane <mark@hackafe.net>
2020-05-23 16:34:45 -0400

    Added plugin installation to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
 import platform
 import sys
+import json
 import os.path
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 pkg_name = 'sideboard'
 __here__ = os.path.abspath(os.path.dirname(__file__))
@@ -41,6 +43,26 @@ if sys.version_info[0] == 2:
     requires = ['CherryPy==3.2.2' if 'cherrypy' in r.lower() else r for r in requires]
 
 if __name__ == '__main__':
+    class InstallWrapper(install):
+        """This wraps the setup.py install command and allows us to pull dependencies before
+        the install runs.
+        """
+        def run(self):
+            # SIDEBOARD_GITPLUGINS is a json list of objects for each plugin
+            # {"plugin_directory": "uber", "git_url": "https://github.com/bitbyt3r/ubersystem.git", "branch": "master"}
+            gitplugins = json.loads(os.environ.get('SIDEBOARD_GITPLUGINS', '[]'))
+            for gitplugin in gitplugins:
+                plugin_directory = ""
+                if 'plugin_directory' in gitplugin:
+                    plugin_directory = os.path.join(__here__, "plugins", gitplugin['plugin_directory'])
+                branch = gitplugin.get("branch", "master")
+                clone_cmd = "git clone --depth=1 --branch {} {} {}".format(branch, gitplugin['git_url'], plugin_directory)
+                os.system(clone_cmd)
+                requirements_file = os.path.join(plugin_directory, "requirements.txt")
+                if os.path.isfile(requirements_file):
+                    os.system("pip install -r {}".format(requirements_file))
+            install.run(self)
+
     setup_requires = {'setup_requires': ['distribute']} if sys.version_info[0] == 2 else {}
     setup(
         name=pkg_name,
@@ -61,6 +83,9 @@ if __name__ == '__main__':
         },
         extras_require={
             'perftrace': ['python-prctl>=1.6.1', 'psutil>=4.3.0']
+        },
+        cmdclass={
+            'install': InstallWrapper
         },
         **setup_requires
     )
